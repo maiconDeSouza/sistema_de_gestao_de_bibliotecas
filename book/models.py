f0107fbe6f8c9abee0feaafcea48263137b93637
Maicon Souza <maicon_souza09@hotmail.com>
2025-03-14 17:48:54 -0300

    end header

--- book/models.py
+++ book/models.py
@@ -12,7 +12,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
         Author, on_delete=models.PROTECT, related_name='book_author')
-    isbn = models.CharField(max_length=16)
+    isbn = models.CharField(max_length=16, unique=True)
     publication_date = models.DateField()
     status = models.CharField(max_length=10, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
